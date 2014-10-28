af18ee745a0ae5152c45b2b44b97921a5e2937b7
atcuan <atcuan@gmail.com>
2014-10-28 10:26:32 +0800

    moditify README, setup.sh

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ CodeSnippets
 ============
 
 backup ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+# Usage
+
+1. git clone https://github.com/atcuan/CodeSnippets
+2. cd CodeSnippets 
+3. ./setup.sh
--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
 SRC_HOME=`pwd`
-ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+ln -s ${SRC_HOME} ~/Library/Developer/Xcode/UserData/CodeSnippets
 echo "setup code snippets done!"
